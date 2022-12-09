8d0aeffa6785c9bb06d01ebb592b6c3f67318be2
hodomodo <94387167+hodomodo@users.noreply.github.com>
2022-12-09 13:45:59 +0000

    Add files via upload

--- Task 2.py
+++ Task 2.py
@@ -27,46 +27,58 @@ while True:
     user_input = input(">")
     Time = []
     if user_input == "END":
-        if not runners_no:
+        if Total_Runners == 0:
             print("No data found.")
-        else:
-            Average_Time = Average_Time / Total_Runners
-            print()
-            print("Total Runners:", Total_Runners)
-            print("Average Time:", convert(Average_Time))
-            print("Fastest Time:", convert(Fastest_Time))
-            print("Slowest Time:", convert(Slowest_Time))
-            print()
-            print("Best Time Here: Runner", Winner)
+        elif Total_Runners > 0:
+            try:
+                Average_Time = Average_Time / Total_Runners
+                print()
+                print("Total Runners:", Total_Runners)
+                print("Average Time:", convert(Average_Time))
+                print("Fastest Time:", convert(Fastest_Time))
+                print("Slowest Time:", convert(Slowest_Time))
+                print()
+                print("Best Time Here: Runner", Winner)
+            except ZeroDivisionError or ValueError or IndexError or TypeError:
+                print("Error, No data to analyse")
+                continue
         break
     elif user_input.isalpha() is True:
         print("Error")
     else:
         try:
-            runners_no.extend(user_input.split("::"))
-            Total_Runners += 1
-            Time.extend(user_input.split("::"))
-            Time_Val = Time[1]
-            Time_Val = int(Time_Val)
-            Average_Time = Average_Time + Time_Val
-            Fastest_Time = int(Fastest_Time)
-        except ValueError or IndexError:
+            runners_no_check = []
+            runners_no_check.extend(user_input.split("::"))
+            if runners_no_check[0] not in runners_no:
+                runners_no.extend(user_input.split("::"))
+                Time.extend(user_input.split("::"))
+                try:
+                    Time_Val = Time[1]
+                except:
+                    print("Error")
+                    continue
+                Time_Val = int(Time_Val)
+                Total_Runners += 1
+                Average_Time = Average_Time + Time_Val
+                Slowest_Time = int(Slowest_Time)
+                try:
+                    if Time_Val < Fastest_Time or Fastest_Time == 0:
+                        Fastest_Time = Time[1]
+                        Winner = Time[0]
+                        Fastest_Time = int(Fastest_Time)
+                    else:
+                        pass
+                    if Time_Val > Slowest_Time or Slowest_Time == 0:
+                        Slowest_Time = Time[1]
+                        Slowest_Time = int(Slowest_Time)
+                    else:
+                        continue
+                except ValueError or IndexError or TypeError:
+                    print("Error")
+                    continue
+            else:
+                print("Error, duplicate runner Number")
+                continue
+        except ValueError or IndexError or TypeError:
             print("Error")
-            try:
-                if Time_Val < Fastest_Time:
-                    Fastest_Time = Time[1]
-                    Winner = Time[0]
-                else:
-                    Fastest_Time = Fastest_Time
-            except ValueError or IndexError:
-                print("Error")
-            try:
-                if Time_Val > Slowest_Time:
-                    Slowest_Time = Time_Val
-                else:
-                    Slowest_Time = Slowest_Time
-            except ValueError or IndexError:
-                print("Error")
-        except ValueError or IndexError:
-            print("Error")
-# Add validation checking it is in xx::xx format. Also ignore inputs other than end. Maybe change current else into elif and new else for any incorrect inputs.
+            continue
