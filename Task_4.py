8853fc1bb54ca4c0a79d4e831a7fa4dc607dd819
hodomodo <94387167+hodomodo@users.noreply.github.com>
2022-12-13 20:49:37 +0000

    Add files via upload

--- Task_4.py
+++ Task_4.py
@@ -29,13 +29,29 @@ if len(sys.argv) > 1:
         with open(sys.argv[1], "r") as f:
             for line in f:
                 decryption = decrypt(line)
-                decrypted_list = ",".join(decrypt(line)).replace(",", "").split(" ")
+                decryption.replace("\n", "")
+                decryption.replace(".", "")
+                decrypted_list = ",".join(decryption).replace(",", "").split(" ")
                 decrypted_list = [s.replace('"', "") for s in decrypted_list]
                 check_1 = decrypted_list[0]
                 decryption_1 = " ".join(decrypted_list)
-                print(checker.check(check_1))
-                if checker.check(check_1) == True or "\n" or '"' in check_1:
+                if checker.check(check_1) == True:
                     print(decryption_1, end="")
-                elif checker.check(check_1) == False or "\n" not in check_1:
+                    for line in f:
+                        decryption = decrypt(line)
+                        decryption.replace("\n", "")
+                        decryption.replace(".", "")
+                        decrypted_list = ",".join(decryption).replace(",", "").split(" ")
+                        decrypted_list = [s.replace('"', "") for s in decrypted_list]
+                        check_1 = decrypted_list[0]
+                        decryption_1 = " ".join(decrypted_list)
+                        print(decryption_1, end="")
+                else:
                     print("Most likely not a caesar cypher at work here")
                     quit()
+    else:
+        print("File could not be found")
+        quit()
+else:
+    print("Please provide a text file to be decrypted")
+    quit()
